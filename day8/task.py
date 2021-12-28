01f9867207e5ef3dd975d1f2d57a8d0bd3819aae
jgolde <jonas.golde@gmail.com>
2021-12-28 18:16:52 +0100

    day 8

--- day8/task.py
+++ day8/task.py
@@ -1,45 +1,76 @@
 def task1():
-    positions = read_input()
-    max_pos = max(positions)
-    min_pos = min(positions)
-
-    best_pos = None
-    best_score = 1000000000000000
-    for pos in range(min_pos, max_pos + 1):
-        current_score = list(map(lambda x: x - pos, positions))
-        current_score = sum([x * (-1) if x < 0 else x for x in current_score])
-        if current_score < best_score:
-            best_score = current_score
-            best_pos = pos
-
-    print(f"Best score {best_score} at position {best_pos}.")
-
+    inputs, outputs = read_input()
+    filtered = list(map(lambda output: list(filter(lambda code: (len(code) in [2, 3, 4, 7]), output)), outputs))
+    total = sum([len(codes) for codes in filtered])
+    print(total)
 
 def task2():
-    positions = read_input()
-    max_pos = max(positions)
-    min_pos = min(positions)
+    inputs, outputs = read_input()
 
-    best_pos = None
-    best_score = 1000000000000000
-    for pos in range(min_pos, max_pos + 1):
-        current_score = list(map(lambda x: x - pos, positions))
-        current_score = [x * (-1) if x < 0 else x for x in current_score]
-        current_score = sum([sum(range(1, dist + 1)) for dist in current_score])
-        if current_score < best_score:
-            best_score = current_score
-            best_pos = pos
+    grand_total = 0
+    for input, output in zip(inputs, outputs):
+        one = set([list(x) for x in input if len(x) == 2][0])
+        seven = set([list(x) for x in input if len(x) == 3][0])
+        four = set([list(x) for x in input if len(x) == 4][0])
+        eight = set([list(x) for x in input if len(x) == 7][0])
 
-    print(f"Best score {best_score} at position {best_pos}.")
+        possible_six_nine_zero = [set(list(x)) for x in input if len(x) in [6]]
+        possible_two_three_five = [set(list(x)) for x in input if len(x) in [5]]
 
+        for candidate in possible_two_three_five:
+            if len(seven.intersection(candidate)) == 3:
+                three = candidate
+                possible_two_three_five.remove(candidate)
+            else:
+                pass
+
+        for candidate in possible_six_nine_zero:
+            if len(four.intersection(candidate)) == 4:
+                nine = candidate
+                possible_six_nine_zero.remove(candidate)
+            else:
+                pass
+
+        for x in possible_six_nine_zero:
+            for y in possible_two_three_five:
+                if len(x.intersection(y)) == 5:
+                    six = x
+                    five = y
+                    possible_six_nine_zero.remove(x)
+                    possible_two_three_five.remove(y)
+
+        zero = possible_six_nine_zero.pop()
+        two = possible_two_three_five.pop()
+
+        solver = [
+            (zero, 0),
+            (one, 1),
+            (two, 2),
+            (three, 3),
+            (four, 4),
+            (five, 5),
+            (six, 6),
+            (seven, 7),
+            (eight, 8),
+            (nine, 9)
+        ]
+
+        final = ''
+        for code in output:
+            for key, value in solver:
+                if set(list(code)) == key:
+                    final += str(value)
+
+        grand_total += int(final)
+
+    print(grand_total)
 
 def read_input():
     with open("input.txt") as file:
-        lines = file.readlines()
-        lines = [line.strip() for line in lines]
-        positions = lines[0].split(',')
-        positions = [int(x) for x in positions]
-        return positions
+        input = list(map(lambda pair:
+                         list(map(lambda codes: codes.split(' '), pair)),
+                         [line.strip().split(' | ') for line in file.readlines()]))
+        return [x[0] for x in input], [x[1] for x in input]
 
 
 if __name__ == "__main__":
